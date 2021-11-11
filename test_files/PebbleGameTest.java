81216a21968cb4625c851c39076b68ac3630a384
afnang <81368254+afnang@users.noreply.github.com>
2021-11-11 03:46:00 +0000

    Update PebbleGameTest.java

--- test_files/PebbleGameTest.java
+++ test_files/PebbleGameTest.java
@@ -1,26 +1,18 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class PebbleGameTest {
 
-    @Test
-    void chooseBag() {
+
+
+    @org.junit.jupiter.api.Test
+    void testGetPlayerCount() {
+        PebbleGame.Player testPlayer = new PebbleGame.Player(0);
+        PebbleGame.Player testPlayer1 = new PebbleGame.Player(1);
+        PebbleGame.Player testPlayer2 = new PebbleGame.Player(2);
+
+        assert(PebbleGame.getPlayerCount()==3);
+
     }
 
-    @Test
-    void loadBag() {
-    }
 
-    @Test
-    void getPlayerCount() {
-    }
-
-    @Test
-    void fileName() {
-    }
-
-    @Test
-    void main() {
-    }
 }
