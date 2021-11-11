21a9562a7cd49e8979626d57fddaea1645352c40
Alperen Türkmen <57950250+AlperenTurkmen@users.noreply.github.com>
2021-11-11 02:25:17 +0000

    Update BlackBagTest.java
    Added tests, finished tests.

--- test_files/BlackBagTest.java
+++ test_files/BlackBagTest.java
@@ -1,18 +1,44 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BlackBagTest {
+    List<Integer> testBlackList = new ArrayList<>();
+    List<Integer> testWhiteList = new ArrayList<>(Arrays.asList(1, 2, 3));
+
+
+    BlackBag testBlackBag = new BlackBag("X", testBlackList);
+    WhiteBag testWhiteBag = new WhiteBag("A", testWhiteList);
+
 
     @Test
-    void getPair() {
+    void testGetPair() {
+        BlackBag[] testEmptyBags = new BlackBag[0];
+        for (BlackBag testBag : testEmptyBags) {
+            assertNotSame("A bag can not be its own pair.",
+                    testBag.getName(),
+                    testBag.getPair().getName());
+        }
+
     }
 
     @Test
-    void setPair() {
+    void testSetPair() {
+        testBlackBag.setPair(testWhiteBag);
+        assertEquals(testBlackBag.getPair(), testWhiteBag, "A pair of a black bag should be set with this method.");
     }
 
     @Test
-    void transferPebbles() {
+    void testTransferPebbles() {
+        testBlackBag.setPair(testWhiteBag);
+        testBlackBag.transferPebbles();
+        assertTrue(testWhiteBag.getPebbles().isEmpty(),"Pebbles from white bag transferred to the paired black bag");
+        assertEquals(testBlackBag.getPebbles(), testWhiteList);
     }
+
+
 }
